3526b781bf00712ff5b10ccdd77dffe6b13a46ba
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-05-11 13:13:35 +0300

    Feat - added ChapterService

---
+++ Book.API/Services/Interfaces/IChapterService.cs
@@ -0,0 +1,9 @@
+﻿using Book.API.Data.Entities;
+
+namespace Book.API.Services.Interfaces
+{
+    public interface IChapterService
+    {
+        Task<Chapters> GetChapterByIdAsync(int id);
+    }
+}
